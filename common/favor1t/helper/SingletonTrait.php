04ba520c339edaf73cd36b6ea71f52401d7b2309
favor1t <favor1t@yandex.ru>
2017-06-18 03:35:51 +0300

    helper

---
+++ common/favor1t/helper/SingletonTrait.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: favor1t
+ * Date: 15.06.2017
+ * Time: 22:52
+ */
+
+namespace app\favor1t\helper;
+
+
+trait SingletonTrait
+{
+    private function __clone(){}
+    private function __wakeup(){}
+}
