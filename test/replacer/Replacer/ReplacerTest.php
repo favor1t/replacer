687f8f4d7d95aa9b0e2d6c8e851cd149da89fca0
favor1t <favor1t@yandex.ru>
2017-06-18 03:43:06 +0300

    ReplaceTest

---
+++ test/replacer/Replacer/ReplacerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: favor1t
+ * Date: 18.06.2017
+ * Time: 3:42
+ */
+
+namespace test\replacer\Replacer;
+
+use favor1t\replacer\Replacer;
+use PHPUnit\Framework\TestCase;
+
+class ReplacerTest extends TestCase
+{
+
+}
--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: favor1t
- * Date: 18.06.2017
- * Time: 3:40
- */
